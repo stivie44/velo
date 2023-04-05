7970173e3e7adec8b630c5a9b5082401198ae20d
Stivie <85050735+stivie44@users.noreply.github.com>
2023-04-05 15:55:31 +0200

    Update velo.py

--- velo.py
+++ velo.py
@@ -1,47 +1,45 @@
 import requests
 
-# Clé API pour l'API JCDecaux
+# Liste des villes disponibles
+villes = ["Bruxelles","Amiens", "Lyon","Marseille","Rouen", "Nantes", "Toulouse","Namur","Santander","Toyama,","Vilnius"]
+
+# Affichage de la liste des villes
+print("Villes disponibles :")
+for ville in villes:
+    print("- " + ville)
+
+# Demande à l'utilisateur de choisir une ville
+ville_choisie = input("Choisissez une ville dans la liste : ")
+
+# Vérification de la validité de la ville choisie
+while ville_choisie not in villes:
+    print("Ville invalide.")
+    ville_choisie = input("Choisissez une ville dans la liste : ")
+
+# Récupération des données de la ville choisie depuis l'API JCDecaux
 api_key = "e0a1bf2c844edb9084efc764c089dd748676cc14"
-
-# Ville pour laquelle nous souhaitons récupérer les données de la station de vélos
-city = "creteil"
-
-# URL de l'API pour la ville sélectionnée
-url = f"https://api.jcdecaux.com/vls/v1/stations?contract={city}&apiKey={api_key}"
-
-# Récupérer les données de l'API
+url = f"https://api.jcdecaux.com/vls/v3/stations?contract={ville_choisie}&apiKey={api_key}"
 response = requests.get(url)
-data = response.json()
 
-# Calculer le nombre total de vélos mécaniques et électriques
-mechanical_bikes = 0
-electric_bikes = 0
-for station in data:
-    try:
-        mechanical_bikes += station["mainStands"]["mechanical"]
-        electric_bikes += station["mainStands"]["electrical"]
-    except KeyError:
-        # Si la clé "mainStands" est absente, ignorez cette station de vélos
-        pass
+# Traitement des données si la requête a abouti
+if response.status_code == 200:
+    data = response.json()
 
-# Calculer le pourcentage de vélos mécaniques et électriques
-total_bikes = mechanical_bikes + electric_bikes
-if total_bikes > 0:
-    mechanical_bikes_percentage = mechanical_bikes / total_bikes * 100
-    electric_bikes_percentage = electric_bikes / total_bikes * 100
+    # trier les stations par pourcentage de vélos disponibles
+    sorted_stations = sorted(data, key=lambda x: x["totalStands"]["availabilities"]["bikes"] / x["totalStands"]["capacity"], reverse=True)
+
+    
+    # afficher les 5 premières stations avec leur pourcentage de vélos disponibles, de vélos électriques et de vélos mécaniques
+    for station in sorted_stations[:5]:
+        available_bike = station["totalStands"]["availabilities"]["bikes"]
+        capacity = station["totalStands"]["capacity"]
+        percentage = int(available_bike / capacity * 100)
+        electric_bikes = station["totalStands"]["availabilities"].get("bikesElectrical", 0)
+        mechanical_bikes = available_bike - electric_bikes
+        electric_percentage = int(electric_bikes / capacity * 100)
+        mechanical_percentage = int(mechanical_bikes / capacity * 100)
+        print(f"{station['name']} - {percentage}% de vélos disponibles ({available_bike}/{capacity} vélos) dont {electric_percentage}% électriques ({electric_bikes}/{capacity} vélos) et {mechanical_percentage}% mécaniques ({mechanical_bikes}/{capacity} vélos)")
+
+# Affichage d'un message d'erreur si la requête a échoué
 else:
-    mechanical_bikes_percentage = 0
-    electric_bikes_percentage = 0
-
-# Afficher les résultats
-print(f"Pourcentage de vélos mécaniques à {city}: {mechanical_bikes_percentage:.2f}%")
-print(f"Pourcentage de vélos électriques à {city}: {electric_bikes_percentage:.2f}%")
-
-# Trier les stations de vélos par nombre de vélos disponibles
-sorted_stations = sorted(data, key=lambda x: x["available_bikes"], reverse=True)
-
-# Afficher les 5 premières stations de vélos
-print(f"\nLes 5 premières stations de vélos à {city} par nombre de vélos disponibles:")
-for i in range(5):
-    station = sorted_stations[i]
-    print(f"{i+1}. {station['name']} - {station['available_bikes']} vélos disponibles")
+    print("Erreur lors de la récupération des données.")
